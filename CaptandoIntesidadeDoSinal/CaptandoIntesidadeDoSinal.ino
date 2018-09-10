845303d4712e654b0c35f302938abc1e01d42722
ale_ads <alessandrokantousian@gmail.com>
2018-09-09 22:47:03 -0300

    Sensor Presença e Access Point

--- CaptandoIntesidadeDoSinal/CaptandoIntesidadeDoSinal.ino
+++ CaptandoIntesidadeDoSinal/CaptandoIntesidadeDoSinal.ino
@@ -1,11 +1,16 @@
 #include "ESP8266WiFi.h"
-
+#define pinoLED D7
+#define pinoSensor D1
  
 void setup()
 {
   Serial.begin(115200);  
   WiFi.mode(WIFI_STA);
   Serial.println();
+  pinMode(pinoLED, OUTPUT);
+  pinMode(pinoSensor, INPUT);  
+  WiFi.mode(WIFI_AP);
+  WiFi.softAP("P1");
  
 }
 
@@ -15,9 +20,18 @@ void scaneiaRede(int numeroSSID){
     Serial.printf("Rede: %s, Distância em dBs: (%d)\n", WiFi.SSID(i).c_str(),WiFi.RSSI(i));
   }
 }
+
+void sensorPresenca(int recebeValor){
+  digitalWrite(pinoLED, LOW);
+  Serial.println(recebeValor);
+  if(recebeValor == 1){
+    digitalWrite(pinoLED, HIGH);
+  }
+}
  
 void loop()
 {
-  scaneiaRede(WiFi.scanNetworks());        
-  delay(60000);                                
+  sensorPresenca(digitalRead(pinoSensor));
+  //scaneiaRede(WiFi.scanNetworks());        
+  delay(500);                                
 }
