346dbfb0b8f1b7a73a6f1b31b932dd4a38b7e9d9
guigajks <guigajks@gmail.com>
2018-09-23 17:21:57 -0300

    modulo netifaces adicionado

--- TCP/server.py
+++ TCP/server.py
@@ -1,21 +1,28 @@
 # source code https://stackoverflow.com/questions/29022329/basic-udp-tcp-program
 
 from threading import Thread
+from netifaces import ifaddresses, AF_INET
 import socket 
 
-LISTENING_IP = socket.gethostbyname(socket.gethostname())
-LISTENING_PORT = 300
+LISTENING_IP = socket.gethostbyname(ifaddresses('wlan0')[AF_INET][0]['addr'])
+LISTENING_PORT = 5000
 BUFFER_SIZE = 1024
 
 def parse_data(data):
-    mac_address = data.split('>')[0]
-    networks = [tuple(dado.split('-')) for dado in data.split(';')[1]]
+    try:
 
-    return (mac_address, networks)
+        mac_address = data.split('>')[0]
+        networks = [tuple(dado.split('-')) for dado in data.split(';')[1]]
+
+        return (mac_address, networks)
+    except:
+        print "Parsing failed"
+        return "Error"
 
 def listen_UDP():
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # IPv4, UDP
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     sock.bind((LISTENING_IP, LISTENING_PORT))
 
     while True:
@@ -26,6 +33,7 @@ def listen_UDP():
 def listen_TCP():
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # IPv4, TCP
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     sock.bind((LISTENING_IP, LISTENING_PORT))
 
     while True:
