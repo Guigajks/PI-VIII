c1468b6a0103d2d634949abbe4bc3de8d8cf9319
caina <caina.camargo@hotmail.com>
2018-10-30 19:42:35 -0300

    correção de erros e ajustes na trilateração

--- Python/calculadora_posicao.py
+++ Python/calculadora_posicao.py
@@ -24,21 +24,57 @@ def trilateracao(coor):
     a = coor[0]
     c = coor[1]
     b = coor[2]
-    
+    #começando a mudar parte 2.0 (tratamento de erro)
+    ok = False
+    #errox é a quantidade de iterações no loop de ajuste da variavel "x"
+    errox = 1
+    #erroy é a quantidade de iterações no loop "not ok", sobre o ajuste do raio de "c"
+    erroy = 1
     x = (math.pow(a[1],2) - math.pow(b[1],2) + math.pow(b[0][0],2))/(2*b[0][0])
-    y = math.sqrt(math.pow(a[1],2) - math.pow(x,2))
-    temp = (math.pow(x-c[0][0],2)) + (math.pow(y-c[0][1],2))
-    temp2 = (math.pow(x-c[0][0],2)) + (math.pow(-y-c[0][1],2))
 
-    if(math.pow(c[1],2) >= (temp-1) and math.pow(c[1],2) <= (temp+1)):
-        pass
+    #looping para ajustes das medidas dos raios a, b e c
+    while(not ok):
+        while(x > a[1]):
+            a[1] = a[1]+1
+            b[1] = b[1]+1
+            x = (math.pow(a[1],2) - math.pow(b[1],2) + math.pow(b[0][0],2))/(2*b[0][0])
+            errox=errox+1
 
-    elif(math.pow(c[1],2) >= (temp2-1) and math.pow(c[1],2) <= (temp2+1)):
-        y = -y
-    
-    else:
-        return "erro",0
+        y = math.sqrt(math.pow(a[1],2) - math.pow(x,2))
+        #as temps são as resoluções da equação "c^2 = (x-c1)^2 + (y-c2)^2", com y sendo positivo e dps negativo
+        temp = (math.pow(x-c[0][0],2)) + (math.pow(y-c[0][1],2))
+        temp2 = (math.pow(x-c[0][0],2)) + (math.pow(-y-c[0][1],2))
 
+        if(math.pow(c[1],2) >= (temp-1) and math.pow(c[1],2) <= (temp+1)):
+            ok = True
+
+        elif(math.pow(c[1],2) >= (temp2-1) and math.pow(c[1],2) <= (temp2+1)):
+            y = -y
+            ok = True
+        
+        else:
+            if((math.pow(c[1],2) - temp) < (math.pow(c[1],2) - temp2)):
+                if(math.pow(c[1],2) > temp):
+                    b[1] = b[1]+0.1
+                    a[1] = a[1]+0.1
+                else:
+                    erroy = erroy+1
+                    b[1] = b[1]-0.1
+                    a[1] = a[1]-0.1
+
+            else:
+                if(math.pow(c[1],2) > temp2):
+                    b[1] = b[1]+0.1
+                    a[1] = a[1]+0.1
+                else:
+                    b[1] = b[1]-0.1
+                    a[1] = a[1]-0.1
+            
+            erroy = erroy+1
+            x = (math.pow(a[1],2) - math.pow(b[1],2) + math.pow(b[0][0],2))/(2*b[0][0])
+
+    #print("errox: ",errox) 
+    #print("erroy: ",erroy)
     return x,y
 
 #2412 frequência comum de roteadores
